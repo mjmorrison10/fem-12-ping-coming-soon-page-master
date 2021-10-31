e6f0d3bcffaff29eae88ed3405bd07ebd19a5428
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-10-30 22:23:12 -0700

    Added notes

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const form = document.querySelector(".email-form");
-const formz = document.querySelector(".email-form");
-const input = document.querySelector(".email");
+const form = document.querySelector(".email-form"); //selects the form
+const formz = document.querySelector(".email-form"); 
+const input = document.querySelector(".email"); //email input
 
-formz.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const email = input.value;
-  const emailRegex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+formz.addEventListener("submit", (e) => { 
+  e.preventDefault(); // prevent the default action of the form
+  const email = input.value; // get the value of the input
+  const emailRegex = 
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
   if (emailRegex.test(email)) {
-    input.parentElement.children[0].style.display = "none";
-    input.parentElement.children[1].classList.remove("error");
+    input.parentElement.children[0].style.display = "none"; //hide the error message
+    input.parentElement.children[1].classList.remove("error"); //remove error class
   } else {
-    input.parentElement.children[0].style.display = "flex";
-    input.parentElement.children[1].classList.add("error");
-  }
-});
+    input.parentElement.children[0].style.display = "flex"; //show the error message
+    input.parentElement.children[1].classList.add("error"); //add error class
+  } 
+});  
 
-// few
